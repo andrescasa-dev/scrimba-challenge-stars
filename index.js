de4a8cae9bf03b7806bc29963ab1e22f80042709
andrescasa-dev <andrescasa.dev@gmail.com>
2022-06-21 19:23:06 -0500

    increment and decrement stars buttons

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
-/* -------------------------------- Remember -------------------------------- */
-/* * Think first: what is problem? and then, how can i solve it?
-/* * Select the proper data structure (or if it doesn't need one)
-/* -------------------------------- Remember -------------------------------- */
-
-
 class Star{
   constructor(data){
     Object.assign(this, data);
   }
   get x(){
-    return Math.sin(this.angle * (Math.PI / 180)) * this.magnitud;
+    return Math.sin(this.angle * (Math.PI / 180)) * this.magnitude;
   }
   get y(){
-    return Math.cos(this.angle * (Math.PI / 180)) * this.magnitud;
+    return Math.cos(this.angle * (Math.PI / 180)) * this.magnitude;
   }
   getHtml(){
     return`<div class="star" style="right: ${this.x + 35}px; bottom: ${this.y + 28}px;">
@@ -23,29 +17,55 @@ class Star{
 
 class App{
   constructor(){
-    this.$universe = document.querySelector('#universe')
-    this.$counter = document.querySelector('#counter')
-  
+    this.$universe = document.querySelector('#universe');
+    this.$counter = document.querySelector('#counter');
+    this.$moreBtn = document.querySelector('#more');
+    this.$lessBtn = document.querySelector('#less');
+    
     this.ringsQuantity = 7;
+    //al the numbers has to be multiple of 2
     this.ringGap = 80;
-    this.magnitud = 50; //also the ring radius 
+    this.initialMagnitude = 40; //also the ring radius+
+    this.magnitude = this.initialMagnitude; 
+    this.starCoefficientList = [1/20,1/18,1/16,1/14,1/12,1/10,1/8];
+    this.starCoefficientIndex = 3;
+    this.starCoefficient = this.starCoefficientList[this.starCoefficientIndex]
 
+    this.addEventListeners();
+    this.loadUniverse();
+  }
+
+  addEventListeners(){
+    this.$moreBtn.addEventListener('click',()=>{
+      const index = (this.starCoefficientIndex < this.starCoefficientList.length - 1) ? ++this.starCoefficientIndex : this.starCoefficientIndex;
+      this.starCoefficient = this.starCoefficientList[index];
+      this.loadUniverse();
+    })
+
+    this.$lessBtn.addEventListener('click',()=>{
+      const index = this.starCoefficientIndex > 0 ? --this.starCoefficientIndex : this.starCoefficientIndex;
+      this.starCoefficient = this.starCoefficientList[index]
+      this.loadUniverse();
+    })
+  }
+
+  loadUniverse(){
     this.starSystems = new Array(this.ringsQuantity).fill(0).map(slot =>{
-      const distance = Math.round(360 / (0.1 * this.magnitud));
-      const newSystem = this.createSystem(distance, this.magnitud)
-      this.magnitud += this.ringGap;
+      const distance = Math.round(360 / ( this.magnitude * this.starCoefficient));
+      const newSystem = this.createSystem(distance, this.magnitude)
+      this.magnitude += this.ringGap;
       return newSystem
     })
+    this.magnitude = this.initialMagnitude;
     this.$counter.innerHTML = this.starSystems.reduce((acc, system)=> acc + system.length,0) + " stars"
     this.displaySystems(this.starSystems);
   }
-  
-  createSystem(distance, magnitud){
+  createSystem(distance, magnitude){
     let stars = [];
     let length = 0;
     while(length < 360){
       length += distance;
-      const newStar = new Star({ angle: length, magnitud})
+      const newStar = new Star({ angle: length, magnitude})
       stars.push(newStar);
     }
     return stars;
@@ -62,6 +82,7 @@ class App{
       divSystem.innerHTML = starSystem.reduce((html, star) => html + star.getHtml() ,'')
       fragment.appendChild(divSystem)
     });
+    this.$universe.innerHTML = '';
     this.$universe.appendChild(fragment)
   }
 }
